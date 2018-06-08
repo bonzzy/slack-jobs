9d10c3119439af99117f1d5b8c20cd3ef3954717
Tomislav Fabeta <tomislav.fabeta@gmail.com>
2018-06-08 18:38:43 +0200

    Initial commit
    First commit with initialized Java Spring template

---
+++ src/main/java/com/example/slackjobs/SlackJobsApplication.java
@@ -0,0 +1,12 @@
+package com.example.slackjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackJobsApplication.class, args);
+	}
+}
