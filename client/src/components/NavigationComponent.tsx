bbec1d4694fa3f014cacfc35a598245790dc30d6
Tomislav Fabeta <tomislav.fabeta@gmail.com>
2018-06-12 01:15:51 +0200

    Creating design
    Added .active modifier on links which depend on active route.

--- client/src/components/NavigationComponent.tsx
+++ client/src/components/NavigationComponent.tsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default class NavigationComponent extends React.Component {
   render() {
+    const activeRouteCssClass = {
+      home: `${location.pathname === '/' ? 'navigation__tabs-link--active' : ''}`,
+      new: `${location.pathname === '/new' ? 'navigation__tabs-link--active' : ''}`,
+    };
     return (
       <nav className={'navigation'}>
         <div className={'navigation__top'}>
@@ -11,8 +15,14 @@ export default class NavigationComponent extends React.Component {
           </Link>
         </div>
         <div className={'navigation__tabs'}>
-          <Link to="/" className={'navigation__tabs-link'}>All jobs</Link>
-          <Link to="/new" className={'navigation__tabs-link'}>Create new</Link>
+          <Link to="/"
+                className={`navigation__tabs-link ${activeRouteCssClass.home}`}>
+            All jobs
+          </Link>
+          <Link to="/new"
+                className={ `navigation__tabs-link ${activeRouteCssClass.new}`}>
+            Create new
+          </Link>
         </div>
       </nav>
     );
