0e69a8677a18910d1276938db458268cd25337ad
Tomislav Fabeta <tomislav.fabeta@gmail.com>
2018-06-15 02:01:55 +0200

    Preparing build version
    Built client new version. Updated main controller.

--- src/main/java/com/example/slackjobs/controllers/IndexController.java
+++ src/main/java/com/example/slackjobs/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = {"/"})
+    @RequestMapping(value = {"/", "/new"})
     public String index() {
         return "index.html";
     }
